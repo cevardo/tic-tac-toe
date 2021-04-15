f277ce0c1bfa23f2a3cf64ed100f9a568430ef81
Chris Evardo <chris.evardo@oddball.io>
2021-04-15 01:06:19 -0400

    Split into client / server

---
+++ server/index.ts
@@ -0,0 +1,8 @@
+import * as express from 'express';
+
+const app = express.application;
+const PORT = 8000;
+app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
